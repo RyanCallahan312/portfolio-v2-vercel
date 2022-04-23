f35cd2c5b44ac7e25c7be4532de830f6e27485eb
Ryan Callahan <RyanCallahan312@gmail.com>
2022-04-23 16:43:37 -0700

    add blog page placeholder

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,14 @@
+import { Center, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Blog: NextPage = () => {
-	return <main></main>;
+	return (
+		<main>
+			<Center pt="10rem">
+				<Heading textColor="white">Coming Soon</Heading>
+			</Center>
+		</main>
+	);
 };
 
 export default Blog;
