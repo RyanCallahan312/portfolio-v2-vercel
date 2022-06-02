08ebc5843156121436f77ee78fe53040cae01bcc
Ryan Callahan <RyanCallahan312@gmail.com>
2022-06-01 21:50:17 -0700

    add seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,38 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
+				{/* eslint-disable-next-line @next/next/no-title-in-document-head */}
+				<title>Portfolio | Dev Ryan</title>
+				<meta
+					name="description"
+					content="Dev Ryan, a full stack developer. A showcase of skills and projects such as the highlight-inator."
+				/>
+				<meta name="og:title" content="Dev Ryan Portfolio" />
+				<meta
+					name="og:description"
+					content="Dev Ryan, a full stack developer. A showcase of all significant works."
+				/>
+				<meta
+					name="og:image"
+					content="https://portfolio.devryan.io/DevRyanLogo.svg"
+				/>
+				<meta name="og:image:alt" content="Dev Ryan Logo" />
+				<meta name="og:url" content="https://portfolio.devryan.io/" />
+				<meta name="og:site_name" content="Dev Ryan Portfolio" />
+				<meta name="og:locale" content="en_US" />
+
+				<meta name="twitter:title" content="Dev Ryan Portfolio" />
+				<meta
+					name="twitter:description"
+					content="Dev Ryan, a full stack developer. A showcase of all significant works."
+				/>
+				<meta
+					name="twitter:image"
+					content="https://portfolio.devryan.io/DevRyanLogo.svg"
+				/>
+				<meta name="twitter:card" content="summary_large_image" />
+
+				<link rel="canonical" href="https://portfolio.devryan.io/" />
 				<link rel="shortcut icon" href="/DevRyanLogo.svg" />
 			</Head>
 			<body
